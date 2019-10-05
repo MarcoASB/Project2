9748865f9e3ef1b7ace429d2a77cebb54f6bb21d
Fernandarangel23 <48704755+Fernandarangel23@users.noreply.github.com>
2019-10-05 11:15:09 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    author = db.Column(db.String())
+    published = db.Column(db.String())
+
+    def __init__(self, name, author, published):
+        self.name = name
+        self.author = author
+        self.published = published
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
